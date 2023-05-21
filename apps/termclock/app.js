550cf1110ea8d2b7e9dc69ced1378337b655b8c4
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-05-21 11:05:25 +0100

    feat: Make it look like a terminal.

--- apps/termclock/app.js
+++ apps/termclock/app.js
@@ -1,19 +1,33 @@
 const X = 160,Y = 140;
 const fontColor = g.theme.dark ? "#0f0" : "#000";
+let paddingY = 2;
+let font6x8At4Size = 32;
+let font6x8At2Size = 18;
+let font6x8FirstTextSize = 4;
+let font6x8DefaultTextSize = 2;
+
+function drawLine(line, pos){
+  if(pos == 1)
+    g.setFont("6x8",font6x8FirstTextSize);
+  else
+    g.setFont("6x8",font6x8DefaultTextSize);
+  let yPos = Bangle.appRect.y + paddingY * (pos - 1) + font6x8At4Size * Math.min(1, pos-1) + font6x8At2Size * Math.max(0, pos-2);
+  g.drawString(">" + line,5,yPos,true);
+};
 
 function draw() {
+  g.reset();
+  let curPos = 1;
+  g.setFontAlign(-1, -1);
+  g.setColor(fontColor);
   var d = new Date();
   var h = d.getHours(),m = d.getMinutes();
   var time = h.toString().padStart(2, 0) + ":" + m.toString().padStart(2,0);
-  g.reset();
-  g.setColor(fontColor);
-  g.setFont("6x8",4);
-  g.setFontAlign(1,1);
-  g.drawString(time,X,Y,true);
-  g.setFont("6x8");
-  g.setFontAlign(0,1);
+  drawLine(time, curPos);
+  curPos++;
   var dateStr = "    "+require("locale").date(d)+"    ";
-  g.drawString(dateStr,g.getWidth()/2,Y+15,true);
+  drawLinw(dateStr,curPos);
+  curPos++;
 }
 
 g.clear();
