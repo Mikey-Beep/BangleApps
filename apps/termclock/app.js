599231005a86de077a9849950c986084094bed1e
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-05-21 17:47:32 +0100

    feat: Add moving average battery data.

--- apps/termclock/app.js
+++ apps/termclock/app.js
@@ -1,26 +1,38 @@
 { // must be inside our own scope here so that when we are unloaded everything disappears
   // we also define functions using 'let fn = function() {..}' for the same reason. function decls are global
 let drawTimeout;
+let batteryTimeout;
 const fontColor=g.theme.dark?"#0f0":"#000";
 let paddingY=2;
 let font6x8At4Size=32;
 let font6x8At2Size=18;
 let font6x8FirstTextSize=4;
 let font6x8DefaultTextSize=2;
+const batteryVals = [];
+
+let updateBattery = function() {
+  batteryVals.push(E.getBattery());
+  if (batteryVals.length > 10) batteryVals.shift();
+  if (batteryTimeout) clearTimeout(batteryTimeout);
+  batteryTimeout = setTimeout(function(){
+    batteryTimeout = undefined;
+    updateBattery();
+  }, 10000 - (Date.now() % 10000));
+}
 
 // Actually draw the watch face
 let draw = function() {
   g.clearRect(0, 24, g.getWidth(), g.getHeight() - 24); // clear whole background (w/o widgets)
   g.setFontAlign(-1,-1);
   g.setColor(fontColor);
-  var date = new Date();
-  var curPos = 1;
+  let date = new Date();
+  let curPos = 1;
   drawTime(date, curPos);
   curPos++;
   drawDate(date, curPos);
   curPos++;
-  //drawBattery(curPos);
-  //curPos++;
+  drawBattery(curPos);
+  curPos++;
   drawLine("",curPos);
   // queue next draw
   if (drawTimeout) clearTimeout(drawTimeout);
@@ -40,33 +52,36 @@ let drawLine = function(line, pos) {
 };
 
 let drawTime = function(date, pos) {
-  var l = Bangle.isLocked()?"*":"";
-  var timeStr = date.getHours().toString().padStart(2,0) + ":" + date.getMinutes().toString().padStart(2,0);
+  let l = Bangle.isLocked()?"*":"";
+  let timeStr = date.getHours().toString().padStart(2,0) + ":" + date.getMinutes().toString().padStart(2,0);
   drawLine(timeStr+l, pos);
 };
 
 let drawDate = function(date, pos) {
-  var dateStr = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2,0) + "-" + date.getDate().toString().padStart(2,0);
+  let dateStr = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2,0) + "-" + date.getDate().toString().padStart(2,0);
   drawLine(dateStr, 2);
 };
 
 let drawBattery = function(pos) {
-  var c = Bangle.isCharging()?" +":"";
-  drawLine("Batt: " + E.getBattery() + "%" + c, pos);
+  let c = Bangle.isCharging()?" +":"";
+  let battSum = batteryVals.reduce((a,b)=>a+b,0);
+  let battMean = (battSum / batteryVals.length) || '?';
+  drawLine("Batt: " + battMean + "%" + c, pos);
 };
 
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
+updateBattery();
 draw();
 Bangle.on('lock', draw);
-// Show launcher when middle button pressed
 Bangle.setUI({
   mode : "clock",
   remove : function() {
-    // Called to unload all of the clock app
     if (drawTimeout) clearTimeout(drawTimeout);
     drawTimeout = undefined;
-    Bangle.removeListener('lock',draw)
+    if (batteryTimeout) clearTimeout(batteryTimeout);
+    batteryTimeout = undefined;
+    Bangle.removeListener('lock',draw);
   }});
 }
